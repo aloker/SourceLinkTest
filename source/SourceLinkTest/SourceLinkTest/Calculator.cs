02d0301e449ca439117a7c8b631443acd44ad7df
Andre Loker <140714+aloker@users.noreply.github.com>
2019-10-06 15:36:10 +0200

    Initial commit

---
+++ source/SourceLinkTest/SourceLinkTest/Calculator.cs
@@ -0,0 +1,16 @@
+namespace SourceLinkTest
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// Calculates the sum of two integers.
+        /// </summary>
+        /// <param name="a">Addend 1</param>
+        /// <param name="b">Addend 2</param>
+        /// <returns>Sum of <paramref name="a"/> and <paramref name="b"/></returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
